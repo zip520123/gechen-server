a18642caa4975e3b8f1d1b02138eb22916517156
蔡祥霖 <zip520123@Woody.local>
2016-05-13 16:03:50 +0800

    test

--- geServer2/models.py
+++ geServer2/models.py
@@ -3,6 +3,7 @@ from django.db import models
 import os
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    pass
 
 class ModelFormWithFileField(models.Model):
     upload = models.FileField()
